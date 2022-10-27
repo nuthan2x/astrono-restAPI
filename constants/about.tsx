8df18876f135bc7162ba5dfa24e8a91e35c60605
Jonathan Emmanuel <88687066+jezenith@users.noreply.github.com>
2022-10-27 14:37:06 +0100

    content update

--- constants/about.tsx
+++ constants/about.tsx
@@ -17,13 +17,28 @@ const about: AboutType[] = [
         },
         title: "About us",
         content:
-            "Astrono is a fun intergalactic NFT Play to earn game with a metaverse that enables multiple forms of player interaction, Competetive Game Modes, and Token Rewards. The game has several modes, including adventure.",
+            "Astrono is a fun intergalactic NFT Play to earn game with a metaverse that enables multiple forms of player interaction, Competetive Game Modes, and Token Rewards.",
         list: [
-            "arena battle, and building, to keep players engaged",
-            ,
+            "Players can build and develop a sophisticated and advanced spaceship.",
+            "players can protect their base from other players.",
+            "Compete in Arena mode or challenge friends to earn crypto rewards for winning matches.\n",
+            "Make in-game purchases for NFTs, Trade, or Stake with rewards",
         ],
     },
-
+    {
+        image: {
+            src: "/images/content/about-char-2.png",
+            height: 50,
+            width: 50,
+            alt: "",
+        },
+        title: "",
+        content:
+            "",
+        list: [
+            "",
+        ],
+    },
 ];
 
 export { about };
