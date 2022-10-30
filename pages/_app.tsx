0a8fc7d44c9e7c8ce0b44ca066e7c8aa81dd7011
Jonathan Emmanuel <88687066+jezenith@users.noreply.github.com>
2022-10-30 12:43:46 +0000

    content update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,6 @@
 import type { AppProps } from "next/app";
+import ReactGA from 'react-ga';
+ReactGA.initialize('UA-247622932-1');
 import "../styles/app.sass";
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -7,6 +9,7 @@ function MyApp({ Component, pageProps }: AppProps) {
             <Component {...pageProps} />
         </>
     );
+
 }
 
 export default MyApp;
